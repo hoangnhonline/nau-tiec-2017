688060bf71c7e003bd5d35b03f33a359b6ada89c
HoangNH <hoangnhonline@gmail.com>
2017-08-07 06:59:03 +0700

    .

--- database/nautiec.sql
+++ database/nautiec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2017 at 09:19 AM
+-- Generation Time: Aug 07, 2017 at 01:57 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
