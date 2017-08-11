344e0dcfaea8fa40ae40552507e3f6d6d5e72df7
HoangNH <hoangnhonline@gmail.com>
2017-08-11 16:37:30 +0700

    .

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="{{ Auth::user()->email != "huongll@tiecngon.vn" ? route('product.index') : route('product.short') }}" class="logo">
+  <a href="{{ route('product.menu') }}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>tiecngon.vn</b></span>
     <!-- logo for regular state and mobile devices -->
@@ -9,11 +9,11 @@
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
     <!-- Sidebar toggle button-->
-    @if(Auth::user()->email != "huongll@tiecngon.vn")
+
     <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
       <span class="sr-only">Toggle navigation</span>
     </a>
-    @endif
+   
     <div class="navbar-custom-menu">
       <ul class="nav navbar-nav">     
         <li class="dropdown user user-menu">
