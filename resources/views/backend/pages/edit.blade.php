71fd6aa58d1d85e30b5f944bd784661501243c12
HoangNH <hoangnhonline@gmail.com>
2017-08-11 13:38:25 +0700

    .

--- resources/views/backend/pages/edit.blade.php
+++ resources/views/backend/pages/edit.blade.php
@@ -16,7 +16,7 @@
   <!-- Main content -->
   <section class="content">
     <a class="btn btn-default btn-sm" href="{{ route('pages.index') }}" style="margin-bottom:5px">Quay lại</a>
-    <a class="btn btn-primary btn-sm" href="{{ route('danh-muc', $detail->slug ) }}" target="_blank" style="margin-top:-6px"><i class="fa fa-eye" aria-hidden="true"></i> Xem</a>
+    <a class="btn btn-primary btn-sm" href="{{ route('parent-cate', $detail->slug ) }}" target="_blank" style="margin-top:-6px"><i class="fa fa-eye" aria-hidden="true"></i> Xem</a>
     <form role="form" method="POST" action="{{ route('pages.update') }}">
     <div class="row">
       <!-- left column -->
