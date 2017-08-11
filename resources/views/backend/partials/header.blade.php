bcb1d3a7c57e917a8c901d6b908b375117205801
HoangNH <hoangnhonline@gmail.com>
2017-08-11 16:38:16 +0700

    .

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="{{ route('product.menu') }}" class="logo">
+  <a href="{{ route('menu.index') }}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>tiecngon.vn</b></span>
     <!-- logo for regular state and mobile devices -->
