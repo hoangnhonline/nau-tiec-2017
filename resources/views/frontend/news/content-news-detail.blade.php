0158673d20ec4c416e02d77567ba0ccf7d14bb58
hoang-nguyen-vn <hoangnhonline@gmail.com>
2018-10-26 13:53:16 +0700

    fox

--- resources/views/frontend/news/content-news-detail.blade.php
+++ resources/views/frontend/news/content-news-detail.blade.php
@@ -1,7 +1,7 @@
 @include('frontend.partials.meta')
 @section('content')
 <div>
-  <img src="{{ URL::asset('assets/images/quytrinh.jpg') }}">
+  <img src="{{ URL::asset('assets/images/quytrinh.jpg') }}" class="img-responsive" alt="banner">
 </div>
 <div class="content-right banggia1">
     <div class="col-md-9">
@@ -17,11 +17,23 @@
         </div>
         <div class="tieude"><h3>Các tin khác</h3></div>
         <div class="box-content">
+          <ul>
           @foreach( $otherArr as $articles)
-            <div style="padding-left:10px; height:auto;">
-              <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}" style="text-decoration:none;"><img src="{{ URL::asset('assets/images/sao.png') }}" border="0"  alt="{!! $articles->title !!}">&nbsp;&nbsp;<span style="font-size:14px; color:#666;">{!! $articles->title !!}</span></a>
-            </div>                
+          <li class="col-sm-6" style="margin-bottom: 10px">                
+              <div class="col-sm-5">
+                <img src="{{ Helper::showImage($articles->image_url) }}" class="img-responsive img-thumbnail">
+              </div>
+              <div class="col-sm-7" style="padding-left: 0px;">
+                  <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}" style="text-decoration:none;"><span style="font-size:14px; color:#666;">{!! $articles->title !!}</span></a>
+                  <p>
+                    {!! $articles->description !!}
+                  </p>
+            </div>    
+          </li>
+                            
           @endforeach
+        </ul>
+        <div class="clearfix"></div>
         </div>
     </div>
     <div class="col-md-3 sidebar">
@@ -62,47 +74,34 @@
                 </div>
             </div>
           </div>
+          <div class="block-sidebar block-news-sb">
+            <div class="block-title">
+              <p class="title">TIN MỚI NHẤT</p>
+            </div>
+            <div class="block-content danhmuc">
+                <div class="wrap-news-list">
+                    <ul class="row product-list">
+                      @foreach($lastestArr as $articles)              
+                      <li class="col-xs-12">
+                        <div class="icons">
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                            <img style="width: 100%" src="{!! Helper::showImage($articles->image_url) !!}" alt="{!! $articles->title !!}">
+                          </a>
+                        </div>
+                        <p class="lastest-title">
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                          {!! $articles->title !!}
+                          </a>
+                        </p>       
+                      </li>           
+                      @endforeach         
+                    </ul>
+                </div>
+            </div>
+          </div>
     </div>
 </div>
 <style type="text/css">
-.sidebar{
-  padding-top: 20px;
-}
-.block-sidebar {
-    margin-bottom: 20px;
-}
-.block-sidebar .block-title .title {
-    font-size: 15px;
-    color: #82bf00;
-    padding-bottom: 5px;
-    border-bottom: 1px solid #82bf00;
-    font-weight: bold;
-    text-transform: uppercase;
-}
-.hotl {
-  color: #ca0808;
-  font-size: 17px;
-  font-weight: bold;
-}
-div.hotline{
-  text-align: center;
-}
-.icons{
-      position: relative;
-    display: inline-block;    
-    text-align: center;    
-    overflow: hidden;
-}
-.danhmuc h5{
-  margin-top: 5px;
-}
-.danhmuc h5 a:hover{
-  color: #82bf00
-}
-#tieude h2{
-  margin-bottom: 15px;
-  font-size:  22px;
-}
-.date{margin-bottom: 15px;}
+
 </style>
 @endsection
--- resources/views/frontend/news/content.blade.php
+++ resources/views/frontend/news/content.blade.php
@@ -1,30 +1,99 @@
 @include('frontend.partials.meta')
 @section('content')
+<div>
+  <img src="{{ URL::asset('assets/images/quytrinh.jpg') }}" class="img-responsive" alt="banner">
+</div>
 <div id="info" class="">
-   <div class="tieude">
-      <h3>Tin tức</h3>
-   </div>
-   <div class="box-content ">
-     
-      @foreach($articlesList as $articles)
-     
-      <div class="tintuc wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
-         <div class="ttlienquan">
-           <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
-                        <img src="{!! Helper::showImage($articles->image_url) !!}" alt="{!! $articles->title !!}">
-         </div>
-         <div class="ttthongtin">
-            <h3><a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">{!! $articles->title !!}</a></h3>
-            <div class="box-news-mota">
-            </div>
-            <div class="box-news-xct"><a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">Xem thêm</a></div>
-         </div>
-         <div class="clear"></div>
+   <div class="col-md-9">
+      <div class="tieude">
+         <h3>Tin tức</h3>
       </div>
-      @endforeach
-      <div class="clear"></div>       
+      <div class="box-content ">
+        
+         @foreach($articlesList as $articles)
+        
+         <div class="tintuc wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+            <div class="ttlienquan">
+              <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                           <img src="{!! Helper::showImage($articles->image_url) !!}" alt="{!! $articles->title !!}">
+            </div>
+            <div class="ttthongtin">
+               <h3><a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">{!! $articles->title !!}</a></h3>
+               <div class="box-news-mota">
+               </div>
+               <div class="box-news-xct"><a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">Xem thêm</a></div>
+            </div>
+            <div class="clear"></div>
+         </div>
+         @endforeach
+         <div class="clear"></div>       
+      </div>
+      <div class="clear"></div>
+      <div class="paging">{{ $articlesList->links() }}</div>
    </div>
-   <div class="clear"></div>
-   <div class="paging">{{ $articlesList->links() }}</div>
+   <div class="col-md-3 sidebar">
+          <div class="block-sidebar block-news-sb">
+            <div class="block-title">
+              <p class="title">HOTLINE</p>
+            </div>
+            <div class="block-content hotline">
+                <div class="wrap-news-list" style="padding-top: 15px;">
+                    <p class="hotl"> 090 2425 068 ( A.Duy )</p>
+                    <p class="hotl">0981 498 043 ( A.Dũng )</p>
+                </div>
+            </div>
+          </div>
+
+          <div class="block-sidebar block-news-sb">
+            <div class="block-title">
+              <p class="title">DANH MỤC</p>
+            </div>
+            <div class="block-content danhmuc">
+                <div class="wrap-news-list">
+                    <ul class="row product-list">
+                      @foreach($tiecList as $articles)              
+                      <li class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="icons">
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                            <img style="width: 100%" src="{!! Helper::showImage($articles->image_url) !!}" alt="{!! $articles->title !!}">
+                          </a>
+                        </div>
+                        <h5>
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                          {!! $articles->title !!}
+                          </a>
+                        </h5>       
+                      </li>           
+                      @endforeach         
+                    </ul>
+                </div>
+            </div>
+          </div>
+          <div class="block-sidebar block-news-sb">
+            <div class="block-title">
+              <p class="title">TIN MỚI NHẤT</p>
+            </div>
+            <div class="block-content danhmuc">
+                <div class="wrap-news-list">
+                    <ul class="row product-list">
+                      @foreach($lastestArr as $articles)              
+                      <li class="col-xs-12">
+                        <div class="icons">
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                            <img style="width: 100%" src="{!! Helper::showImage($articles->image_url) !!}" alt="{!! $articles->title !!}">
+                          </a>
+                        </div>
+                        <p class="lastest-title">
+                          <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">
+                          {!! $articles->title !!}
+                          </a>
+                        </p>       
+                      </li>           
+                      @endforeach         
+                    </ul>
+                </div>
+            </div>
+          </div>
+    </div>
 </div>
 @endsection
